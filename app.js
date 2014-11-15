0a7f76c1f9afa8f6454f8b1badcd9baad89dd78e
johnvarv <john_varv2005@yahoo.gr>
2014-11-15 17:05:02 +0200

    Revert "Revert "mdpdf""
    This reverts commit a287d1f4d1b8633f692aba1e4bf09486ee313854.

---
+++ app.js
@@ -0,0 +1,41 @@
+//required modules
+var fs = require("fs");
+var express = require("express");
+var https = require("https");
+var bodyParser = require("body-parser");
+var markdownpdf = require("markdown-pdf")
+var path = require("path");
+
+//defining express app
+var app = express();
+
+//defining routes
+var downpage = require("./routes/downpage");
+var downloadfile = require("./routes/downloadfile");
+var index = require("./routes/index");
+
+console.log("Starting");
+
+//setting html view engine and .html files default folder
+app.engine("html", require("ejs").renderFile);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
+
+//using body-parser middleware for parsing JSON response from posting the form
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//define path of static files
+// app.use(express.static(__dirname + "/public"));
+
+//getting index.html
+app.get("/", index);
+
+//posting donwpage.html
+app.post("/downpage", downpage);
+
+//route for downloading document.pdf
+app.get("/downloadfile", downloadfile);
+
+//defining listening port for the web server
+app.listen(3000);
